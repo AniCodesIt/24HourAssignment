6bc3b04fad41190ea348109d1c0752039d49e4d1
anicodesit <ani.kulka@gmail.com>
2021-02-02 21:48:43 -0500

    Add project files.

---
+++ 24HourAssingment/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_24HourAssignment.Startup))]
+
+namespace _24HourAssignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            ConfigureAuth(app);
+        }
+    }
+}
